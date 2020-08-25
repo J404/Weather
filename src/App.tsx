f425153a640827e5789a510c647d56267f1ef5f4
Jimmy <jimmystry@gmail.com>
2020-08-25 01:30:16 -0400

    Changed body font

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App text-white bg-gray-900 w-screen h-screen">
+      <h3 className="text-5xl font-header">Weather</h3>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,10 @@ module.exports = {
     future: {
         removeDeprecatedGapUtilities: true,
     },
+    theme: {
+        fontFamily: {
+            'header': ['Roboto'],
+            'body': ['Roboto'],
+        }
+    }
 };
