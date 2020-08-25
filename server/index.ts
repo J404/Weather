b737ff034f768d7ce61086098f9919571c8dd500
Jimmy <jimmystry@gmail.com>
2020-08-25 14:38:19 -0400

    Server queries weather info and sends through weather route

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,31 @@
-console.log('hello');
+import * as Express from 'express';
+
+import * as fetch from 'node-fetch';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const app = Express();
+const PORT = process.env.PORT || 5000;
+
+app.use(Express.json());
+
+try {
+    app.use(Express.static('../build'));
+} catch(err) {
+    console.log('Build folder not yet created');
+}
+
+app.get('/weather', async (req: Express.Request, res: Express.Response) => {
+    const key = process.env.API_KEY;
+
+    // For now, lat & lon are hardcoded
+    // TODO: send lat/lon through route params
+    const apiRes: Promise<Response> = await fetch(`https://api.darksky.net/forecast/${key}/41.505550,41.505550`, {});
+    const { currently, hourly } = await (await apiRes).json();
+
+    res.json({ data: { currently, hourly }});
+});
+
+app.listen(PORT);
+console.log(`Listening on port ${PORT}`);
