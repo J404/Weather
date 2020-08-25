f877b40e5cf1fbccb8224edccac244bfc7c46692
Jimmy <jimmystry@gmail.com>
2020-08-25 15:10:18 -0400

    Moved types to a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import * as WeatherTypes from './weatherTypes';
+
 import Subtitle from './Subtitle';
 import WeatherBox from './WeatherBox';
 
-interface HourlyWeatherList {
-  summary: string;
-  icon: string;
-  data: Weather[];
-}
-
-interface Weather {
-  time: number;
-  summary: string;
-  icon: string;
-  precipProbability: number;
-  temperature: number;
-}
-
-interface DailyWeather {
-  summary: string;
-  data: DayWeather[];
-}
-
-interface DayWeather {
-  time: number;
-  summary: string;
-  icon: string;
-  precipIntensity: number;
-  precipProbability: number;
-  precipType: string;
-  apparentTemperatureHigh: number;
-  apparentTemperatureLow: number;
-}
-
-interface WeatherData {
-  currently: Weather;
-  hourly: HourlyWeatherList;
-  daily: DailyWeather;
-}
-
-interface APIResponse {
-  data: WeatherData;
-}
-
 function App() {
-  const [weatherData, updateWeather] = useState<WeatherData | null>(null);
+  const [weatherData, updateWeather] = useState<WeatherTypes.WeatherData | null>(null);
 
   useEffect(() => {
     const getWeatherData = async () => {
       const serverResponse = await fetch('http://localhost:5000/weather');
-      const serverData: APIResponse = await serverResponse.json();
+      const serverData: WeatherTypes.APIResponse = await serverResponse.json();
 
       updateWeather(serverData.data);
     }
---
+++ src/weatherTypes.ts
@@ -0,0 +1,39 @@
+export interface HourlyWeatherList {
+    summary: string;
+    icon: string;
+    data: Weather[];
+}
+
+export interface Weather {
+    time: number;
+    summary: string;
+    icon: string;
+    precipProbability: number;
+    temperature: number;
+}
+
+export interface DailyWeather {
+    summary: string;
+    data: DayWeather[];
+}
+
+export interface DayWeather {
+    time: number;
+    summary: string;
+    icon: string;
+    precipIntensity: number;
+    precipProbability: number;
+    precipType: string;
+    apparentTemperatureHigh: number;
+    apparentTemperatureLow: number;
+}
+
+export interface WeatherData {
+    currently: Weather;
+    hourly: HourlyWeatherList;
+    daily: DailyWeather;
+}
+
+export interface APIResponse {
+    data: WeatherData;
+}
