fae11f4471dbf00415d3a530cf66be962cd84a26
Jimmy <jimmystry@gmail.com>
2020-08-26 10:26:28 -0400

    Added component to display hourly weather

---
+++ src/HourlyWeather.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HourlyWeather: React.FC = () => {
+    return (
+        <div className='HourlyWeather grid grid-cols-4'>
+            
+        </div>
+    );
+}
+export default HourlyWeather;
