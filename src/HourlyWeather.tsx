f43e0f4caee6bf281f3ea39633e35e118c9b7b7f
Jimmy <jimmystry@gmail.com>
2020-08-26 11:59:19 -0400

    Fixed times

--- src/HourlyWeather.tsx
+++ src/HourlyWeather.tsx
@@ -7,14 +7,14 @@ import * as WeatherTypes from './weatherTypes';
 const HourlyWeather: React.FC<WeatherTypes.HourlyWeatherList> = props => {
     const currentHour = new Date().getHours();
 
-    const hourlySections = [props.data[1], props.data[5], props.data[9], props.data[13]];
+    const hourlySections = [props.data[1], props.data[3], props.data[5], props.data[7]];
 
     return (
         <div className='HourlyWeather grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4'>
             {
                 hourlySections.map((hourWeather, i) => <div key={i}>
                     <WeatherIcon icon={hourWeather.icon} height={72} width={61.8}/>
-                    <p>{`${currentHour + i + 1}:00`}</p>
+                    <p>{`${currentHour + (i * 2) + 1}:00`}</p>
                     <p>{`${Math.floor(hourWeather.temperature)}F`}</p>
                     <p>{`${Math.floor(hourWeather.precipProbability * 100)}% chance of ${hourWeather.precipType}`}</p>
                 </div>)
