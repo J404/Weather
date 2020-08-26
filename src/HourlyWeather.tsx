9da229e1b724259587e20fbb5f0c512a55368bde
Jimmy <jimmystry@gmail.com>
2020-08-26 18:58:33 -0400

    Made function to get the time in hours

--- src/HourlyWeather.tsx
+++ src/HourlyWeather.tsx
@@ -9,12 +9,21 @@ const HourlyWeather: React.FC<WeatherTypes.HourlyWeatherList> = props => {
 
     const hourlySections = [props.data[1], props.data[3], props.data[5], props.data[7], props.data[9]];
 
+    const getTimeFromIndex = (i: number): string => {
+        let hour = currentHour + (i * 2) + 1;
+
+        while (hour > 24)
+            hour -= 24;
+
+        return `${hour}:00`;
+    }
+
     return (
         <div className='HourlyWeather grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-5'>
             {
                 hourlySections.map((hourWeather, i) => <div key={i}>
                     <WeatherIcon icon={hourWeather.icon} height={72} width={61.8}/>
-                    <p>{`${currentHour + (i * 2) + 1}:00`}</p>
+                    <p>{getTimeFromIndex(i)}</p>
                     <p>{`${Math.floor(hourWeather.temperature)}F`}</p>
                     {(hourWeather.precipType) ? 
                         <p>{`${Math.floor(hourWeather.precipProbability * 100)}% chance of ${hourWeather.precipType}`}</p>
