e951864cb9f7adb867f1b818afbb432833d4dc21
Jimmy <jimmystry@gmail.com>
2020-08-25 23:24:45 -0400

    Refactored DailyWeather

--- src/DailyWeather.tsx
+++ src/DailyWeather.tsx
@@ -8,18 +8,30 @@ interface Props {
     dailyWeather: WeatherTypes.DailyWeather;
 }
 
+const getDaysOfTheWeek = (): string[] => {
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const today = new Date().getDay();
+
+    for (let i = 0; i < today + 1; i++) {
+        const removedDay = days.shift();
+        days.push(removedDay as string);
+    }
+
+    return days.slice(0, 5);
+}
+
 const DailyWeather: React.FC<Props | undefined> = props => {
+    const days = getDaysOfTheWeek();
+
     return (
         <div className='DailyWeather'>
         { (props.dailyWeather && props.dailyWeather.summary && props.dailyWeather.data) ? (
             <div>
                 <p>{props.dailyWeather.summary}</p>
                 <div className='grid xsm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5'>
-                    <WeatherBox dayName='Monday' data={props.dailyWeather.data[1]}/>
-                    <WeatherBox dayName='Tuesday' data={props.dailyWeather.data[2]}/>
-                    <WeatherBox dayName='Wednesday' data={props.dailyWeather.data[3]}/>
-                    <WeatherBox dayName='Thursday' data={props.dailyWeather.data[4]}/>
-                    <WeatherBox dayName='Friday' data={props.dailyWeather.data[5]}/>
+                    {
+                        days.map((dayName, i) => <WeatherBox dayName={dayName} data={props.dailyWeather.data[i + 1]}/>)
+                    }
                 </div>
             </div>
         ) : (
